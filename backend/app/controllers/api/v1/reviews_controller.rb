ff99fb9e426b8c8da2b926aa3ee728c958e69768
Veenie <raveen08@gmail.com>
2021-02-17 15:03:19 -0800

    added create and strong params

--- backend/app/controllers/api/v1/reviews_controller.rb
+++ backend/app/controllers/api/v1/reviews_controller.rb
@@ -7,7 +7,23 @@ class Api::V1::ReviewsController < ApplicationController
     
     #all reviews in json format, don't need instance variables (no erb display)
 
-    
+    def create
+        review = Review.new(review_params)
+        if review.save
+            render json: review
+        else
+            render json: {errors: review.errors.full_messages}
+        end
+    end        
+
+
+    private
+
+    def review_params
+        params.require(:review).permit(:reviewer, :header, :body, :movie_id)
+    end
+
+    #permissable attributes, review is top level hash required
 
 
 
