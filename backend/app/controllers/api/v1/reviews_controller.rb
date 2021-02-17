f7a1a41f68d2ff8eef797ba28df49c5068643dc3
Veenie <raveen08@gmail.com>
2021-02-17 14:12:30 -0800

    index in controller

--- backend/app/controllers/api/v1/reviews_controller.rb
+++ backend/app/controllers/api/v1/reviews_controller.rb
@@ -1,2 +1,15 @@
 class Api::V1::ReviewsController < ApplicationController
+
+    def index
+        reviews = Review.all 
+        render json: reviews
+    end
+    
+    #all reviews in json format, don't need instance variables (no erb display)
+
+    
+
+
+
+
 end
