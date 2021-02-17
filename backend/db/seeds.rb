3d60f5069f5a754c4b9235b4259aeec4719f9d17
Veenie <raveen08@gmail.com>
2021-02-17 12:12:18 -0800

    added associations, seed data prior to migrate

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Movie.create(title: 'The Dark Knight')
+Movie.create(title: 'The Dark Knight Rises')
+Movie.create(title: 'Batman Begins')
