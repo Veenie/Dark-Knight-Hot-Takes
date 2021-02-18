08a976f23b34dd4c0cc4f44249a43b2225d4ee5c
Veenie <raveen08@gmail.com>
2021-02-18 14:03:57 -0800

    added js files, updated index html, index js has dom content loaded

---
+++ frontend/index.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("This is really cool!");
+  });
+
+//Dom content loaded explainer  
+
+//console.log("test")
+//To test script tag is connecting the file to index.html
