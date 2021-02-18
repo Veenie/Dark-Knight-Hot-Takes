eefd6fa50d9152ac33907598d3e7b20fc01b1c1a
Veenie <raveen08@gmail.com>
2021-02-18 15:30:25 -0800

    made get reviews function, tested putting data on dom

--- frontend/index.js
+++ frontend/index.js
@@ -2,14 +2,21 @@
 const endpoint = "http://localhost:3000/api/v1/reviews"
 
 document.addEventListener("DOMContentLoaded", function() {
-    fetch(endpoint)
-    .then(response => response.json())
-    .then(r => {
-        console.log(r);
-        })
+    getReviews()
   });
 
 //Dom content loaded explainer  
+
+function getReviews() {
+  fetch(endpoint)
+    .then(response => response.json())
+    .then(r => r.forEach(element => {
+        const p = `<h3>${element.reviewer}</h3>`;
+        document.querySelector('body').innerHTML += p;
+    }))
+}
+
+//fetch review data from our endpoint, change data to json
 
 //console.log("test")
 //To test script tag is connecting the file to index.html
