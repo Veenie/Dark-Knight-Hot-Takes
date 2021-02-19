6aa7b0176e3df53632f27a986ea5b9c4f21c71b1
Veenie <raveen08@gmail.com>
2021-02-18 20:21:57 -0800

    got our values from form, ready for fetching

--- frontend/index.js
+++ frontend/index.js
@@ -28,14 +28,25 @@ function getReviews() {
     }))
 }
 
-
+//fetch review data from our endpoint, change data to json
 
 function formHandler(e){
     e.preventDefault()
-    console.log(e)
+    const name = document.querySelector('#name').value
+    const movie = parseInt(document.querySelector('#movie').value)
+    const header = document.querySelector('#header').value
+    const body = document.querySelector('#body').value
+    postFetch(name, movie, header, body)
+    //debugger
+    //console.log(e)
 }
 
-//fetch review data from our endpoint, change data to json
+//Used debugger and browser console to find and select the form values entered
+//We parseInt to go from string to integer, which is what a movie_id is
+
+function postFetch(name, movie, header, body){
+    
+}
 
 //console.log("test")
 //To test script tag is connecting the file to index.html
