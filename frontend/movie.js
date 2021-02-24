6f3f22bc2b62e8aeb9f8b7b08bc7db8e198ccb9d
Veenie <raveen08@gmail.com>
2021-02-24 12:20:14 -0800

    commented out unused js file, got rid of related script tag in index.html

--- frontend/movie.js
+++ frontend/movie.js
@@ -1,13 +1,13 @@
-class Movie {
-    constructor(movie) { 
-      this.id = movie.id;
-      this.title = movie.title;
-      Movie.all.push(this);
-    }
+// class Movie {
+//     constructor(movie) { 
+//       this.id = movie.id;
+//       this.title = movie.title;
+//       Movie.all.push(this);
+//     }
 
-    static findById(id) {
-        return this.all.find(m => m.id === id);
-      }
-}
+//     static findById(id) {
+//         return this.all.find(m => m.id === id);
+//       }
+// }
 
-Movie.all = [];
+// Movie.all = [];
